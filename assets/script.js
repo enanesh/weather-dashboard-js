853d24809ca3ba3c1ee58d464c178f0e1065b0d5
enanesh <edna.ganesha.gr@gmail.com>
2022-11-03 01:19:58 -0700

    local storage and changes to the UI

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,64 @@
+//VARIABLES 
 
-function getAPI() {
-    
-    var cityUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=(mexico&limit)=5&appid=fa199b1fa16945296c4472000a55f603';
+var apiString= "fa199b1fa16945296c4472000a55f603";
+var city = "";
+var lon = "";
+var lat = "";
 
-        // fetch(cityUrl)
-        //     .then(function (response) {
-        //         return response.json();
-        //     })
-        //     .then(function (data) {
-        //         //Using console.log to examine the data
-        //         console.log(data);
-        //         for (var i = 0; i < data.length; i++) {
-        //             //Creating a h3 element and a p element
-        //             var userName = document.createElement('h3');
-        //             var userUrl = document.createElement('p');
 
-        //             //Setting the text of the h3 element and p element.
-        //             userName.textContent = data[i].login;
-        //             userUrl.textContent = data[i].url;
+//THIS GETS THE 5 DAY FORECAST 
+var ipaCall5day = "api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&ctn=6&appid=fa199b1fa16945296c4472000a55f603"
 
-        //             //Appending the dynamically generated html to the div associated with the id="users"
-        //             //Append will attach the element as the bottom most child.
-        //             usersContainer.append(userName);
-        //             usersContainer.append(userUrl);
-        //         }
-        //     });
-    
-    console.log(cityUrl)
+
+//maybe i need to limit this parameters to show only the first element in the array 
+var ipaCallNameCity = "http://api.openweathermap.org/geo/1.0/direct?q=london&appid=fa199b1fa16945296c4472000a55f603"
+
+
+
+var queryUrl = "";
+
+
+
+
+//LOCAL STORAGE FOR USER INPUT 
+
+function inputStorage() {
+    $("#button").click(function (e) {
+        e.preventDefault();
+        var city = $("#search-input")
+        var item = JSON.parse(localStorage.getItem("cities"));
+        if (item == null) {
+            item = [city.val()]
+        }
+        else
+        {
+            if (item.includes(city.val())) {
+            
+            } else {
+                item.push(city.val());
+            }  
+        }
+        localStorage.setItem("cities", JSON.stringify(item));
+        
+        console.log(city);
+        
+    });
+
+
 }
 
+inputStorage()
 
-getAPI();
+
+// create the for loop for the buttons 
+
+//store the api (lon,lat) variables for the ne api call 
+
+//fix the content into the boxes 
+
+
+
+
+
+
+
