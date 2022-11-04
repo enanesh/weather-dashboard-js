7efaf3af05b256c6b7a135ee8dbe911ca6a3daee
enanesh <edna.ganesha.gr@gmail.com>
2022-11-04 02:27:30 -0700

    Api Call

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ var lat = "";
 var ipaCall5day = "api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&ctn=6&appid=fa199b1fa16945296c4472000a55f603"
 
 //THIS GETS THE NAME OF THE CITY
-var ipaCallNameCity = "http://api.openweathermap.org/geo/1.0/direct?q=london&appid=fa199b1fa16945296c4472000a55f603"
+
 
 
 var queryUrl = "";
@@ -37,12 +37,12 @@ function inputStorage() {
 
         console.log(city);
         citiesButtons();
-
+       
     });
 
 
 }
-
+cityUrl();
 inputStorage()
 citiesButtons();
 
@@ -58,10 +58,10 @@ function citiesButtons() {
 
     for (i = 0; i < citiesLocalstorage.length; i++) {
 
-
         var btn = $('<button/>', {
             text: citiesLocalstorage[i],
             id: citiesLocalstorage[i],
+            class: "container btn btn-secondary"
         });
         var list = $('<li>');
         list.append(btn);
@@ -77,10 +77,46 @@ function citiesButtons() {
 
 function cityUrl() {
     
+    var UrlArray = JSON.parse(localStorage.getItem("cities"));
 
-    console.log()
+    console.log(UrlArray);
+
+    var city= UrlArray[UrlArray.length - 1];
+     
+    console.log(city);
+
+    var ApiCallNameCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiString;
+
+
+
+    function getApi(requestUrl) {
+        fetch(requestUrl)
+            .then(function (response) {
+                console.log(response.status);
+                //  Conditional for the the response.status.
+                if (response.status !== 200) {
+                    // Place the response.status on the page.
+                    responseText.textContent = response.status;
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                // Make sure to look at the response in the console and read how 404 response is structured.
+
+
+                
+                console.log(data);
+            });
+    }
+
+    getApi(ApiCallNameCity);
+
 
     
+
+
+
+
 }
 
 
@@ -89,21 +125,5 @@ function cityUrl() {
 
 
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
-        .then(function (response) {
-            console.log(response.status);
-            //  Conditional for the the response.status.
-            if (response.status !== 200) {
-                // Place the response.status on the page.
-                responseText.textContent = response.status;
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            // Make sure to look at the response in the console and read how 404 response is structured.
-            console.log(data);
-        });
-}
 
-getApi(badRequestUrl);
+
